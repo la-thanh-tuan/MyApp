fb60e5295354f59bc3c3c24a21abc81a6e22e1d2
La Thanh Tuan <lathanhtuan@outlook.com>
2020-11-30 21:48:34 +0700

    Add entities

---
+++ src/MyApp.API/Data/Interfaces/Interface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApp.API.Data.Interfaces
+{
+    interface Interface
+    {
+    }
+}
---
+++ src/MyApp.ViewModels/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyApp.ViewModels
+{
+    public class Class1
+    {
+    }
+}
